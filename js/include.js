8679a2f73c3dbb2b5232c677b46fc9acef31e81a
Michel Albonico <bsd.albonico@gmail.com>
2025-02-15 17:33:08 -0300

    footer

--- js/include.js
+++ js/include.js
@@ -1,11 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    //loadHTML("header-container", "header.html");
+    loadHTML("footer-container", "footer.html");
+});
+
 function loadHTML(id, file) {
     fetch(file)
-        .then(response => response.text())
-        .then(data => document.getElementById(id).innerHTML = data)
-        .catch(error => console.error("Error loading the file: ", error));
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        })
+        .catch(error => console.error("Error loading file:", error));
 }
-
-window.onload = function() {
-   // loadHTML("header-container", "header.html");
-    loadHTML("footer-container", "footer.html");
-};
